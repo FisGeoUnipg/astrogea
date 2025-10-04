f3be2021da6a1a552dac8dd88a9b2939bc775683
Valter Amorini <amorini@artimarziali.it>
2025-10-04 20:25:16 +0200

    Update repository URLs to FisGeoUnipg/astrogea and bump version to 0.1.13

--- astrogea/__init__.py
+++ astrogea/__init__.py
@@ -2,7 +2,7 @@ from .core import process_crism_file, envi_to_xarray_wcs, band_parameters_mafic,
 from .spectral_wrapper import SpectralArrayWrapper
 from .wcs_utils import parse_envi_map_info_list, create_wcs_from_parsed_info, create_wcs_header_dict
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 __all__ = [
     "process_crism_file",
